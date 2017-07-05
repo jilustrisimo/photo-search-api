56feea618ddcffb3455ffcc1c442183b6e7419e0
github username <jilustrisimo@users.noreply.github.com>
2017-07-05 15:21:32 -0400

    added relationship for :favorites, added min. length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-
+  has_many :favorites
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
   validates_email_format_of :email
 
-
+  validates :password, length: { minimum: 8 }, on: :create
 end
