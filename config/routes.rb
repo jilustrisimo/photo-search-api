e3a65e33db859e96ed66f6620686d649069382a1
github username <jilustrisimo@users.noreply.github.com>
2017-07-18 15:52:06 -0400

    added ruotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do  
+
+      # /api/v1/users
+      resources :users, only: [:create]
+
+      # /api/v1/auth
+      post '/auth', to: "auth#login"
+      post '/auth/refresh', to: "auth#refresh"
+
+    end
+  end
 end
