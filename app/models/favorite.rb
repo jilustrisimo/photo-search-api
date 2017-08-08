20c7b968745f25cfdccffc29d09591ca94947950
github username <jilustrisimo@users.noreply.github.com>
2017-08-08 12:01:40 -0400

    fixed typo

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :photographer, :image_url, :profile_url
-  validate :image_url, :profile_url, uniqueness: :true
+  validates :image_url, :profile_url, uniqueness: :true
 end
